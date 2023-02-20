6840e23d0631fa84e9002a5692a85c7bd8ef8541
GenaPC <lotog@yandex.ru>
2023-02-20 11:37:43 +0300

    Example005_ConditionlfElse

---
+++ Example005_ConditionlfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿//Варианты приветствий после введения имени//
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "наташа")
+{
+    Console.WriteLine("Наташа, ты где была? Мы все просрали!");
+}
+else
+{
+    Console.Write("Привет, !");
+    Console.WriteLine(username);
+}
