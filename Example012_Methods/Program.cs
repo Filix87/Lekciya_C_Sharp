d4b6e69aca6044e67decb257bc99bcede4d5078f
GenaPC <lotog@yandex.ru>
2023-02-20 17:31:14 +0300

    new commit 012

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -98,3 +98,39 @@ Console.WriteLine(newText);
 System.Console.WriteLine();
 newText = Replace(newText, 'к', 'К');
 Console.WriteLine(newText);
+
+// Упорядочивание данных массива
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int cont = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Lingth - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
