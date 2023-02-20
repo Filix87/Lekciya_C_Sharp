c088013b0eaa9001a23767f1a6937394dc1a8198
GenaPC <lotog@yandex.ru>
2023-02-20 17:10:52 +0300

    new commit 012

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -39,17 +39,62 @@ Console.WriteLine(year);
 
 // Вид 4 - что-то принимают и что-то возвращают
 
-string Method4(int count, string c)
+string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-    while (i < count)
+    for (int i = 0; i < count; i++)
     {
         result = result + text;
-        i++;
     }
     return result;
 }
 
 string res = Method4(10, "asdf");
 Console.WriteLine(res);
+
+//пример использования for как цикл внутри цикла (получиться таблица умножения)
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+
+/* Работа с текстом
+Дан текст. В тексте нужно все пробелы заменить черточками,
+Маленькие буквы "к" заменить большими "К",
+А большие "С" заменить маленькими "с".
+
+Ясна ли задача?*/
+
+string text = "– Я думаю, – сказал князь, улыбаясь, – что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероду,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречевыю Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+System.Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
